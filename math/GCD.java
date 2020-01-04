1499a0070cf44e7d4f31f0d3f56e0ca988d09f1c
Piotr Malek <piomalek@gmail.com>
2020-01-04 23:19:42 +0100

    math problems - gcd

---
+++ math/GCD.java
@@ -0,0 +1,20 @@
+public class GCD {
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 256;
+        int b = 144;
+
+        System.out.println(String.format("gcd(%d, %d) = %d", a, b, gcd(a, b)));
+
+        return;
+    }
+}
---
+++ math/README.md
@@ -0,0 +1,3 @@
+# Math problems
+
+**GCD - Greatest Common Devisor**
